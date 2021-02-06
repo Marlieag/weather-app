2d666bc5e0633fdac5010ffee9fa40417523fcc2
Marlieag <Marlieag@users.noreply.github.com>
2021-02-06 14:45:49 -0700

    Hours function added

--- src/index.js
+++ src/index.js
@@ -20,6 +20,8 @@ highTemp.innerHTML= `${high}°`;
 lowTemp.innerHTML=`${low}°`;
 let dateElement= document.querySelector("#today-date");
 dateElement.innerHTML= formatDate(response.data.dt * 1000);
+let timeElement= document.querySelector("#current-time");
+timeElement.innerHTML= formatHours(response.data.dt * 1000);
 
 let humidity= response.data.main.humidity;
 let humid= document.querySelector("#currentH");
@@ -108,7 +110,20 @@ let month= months[date.getMonth()];
 return `${day}, ${month} ${today}`;
 }
 
+//Time
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
+  return `${hours}:${minutes}`;
+}
 
 
 
