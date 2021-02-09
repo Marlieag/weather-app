6e8e8df991401282b4057fea32af91da5ffd6af5
Marlieag <Marlieag@users.noreply.github.com>
2021-02-08 23:22:50 -0700

    Fixed feels like, min and max temp

--- src/index.js
+++ src/index.js
@@ -26,11 +26,11 @@ timeElement.innerHTML= formatHours(response.data.dt * 1000);
  
 let feelsLike= Math.round(response.data.main.feels_like);
 let feels= document.querySelector("#feels");
-feels.innerHTML=`Feels Like: ${feelsLike}`;
 let humidity= response.data.main.humidity;
 let humid= document.querySelector("#currentH");
 humid.innerHTML= `Humidity: ${humidity}%`;
 
+feels.innerHTML=`Feels Like: ${feelsLike}°`;
 let pressure= response.data.main.pressure;
 let press= document.querySelector("#currentP");
 press.innerHTML=`Pressure: ${pressure}in`;
@@ -49,8 +49,8 @@ forcastElemnent.innerHTML=null;
 let nextFiveForecast= null;
 
 
+nextFiveForecast = response.data.daily[index];
 for (let index = 1 ;index < 6; index++) {
-    nextFiveForecast = response.data.daily[index];
     forecastHour= response.data.hourly[index].temp;
 forcastElemnent.innerHTML += 
 `  <div class="col-sm-2.5">
@@ -58,10 +58,10 @@ forcastElemnent.innerHTML +=
               <div class="card-body">
                 <h5 class="card-title">
                   <span class="weekday">
-                  ${formatDay(nextFiveForecast.dt*1000)} 
+                  ${formatDay(nextFiveForecast.dt)} 
                   </span>
                   <br/>
-                  ${futureDate(nextFiveForecast.dt *1000)}
+                  ${futureDate(nextFiveForecast.dt)}
                 </h5>
                     <img
                   src="http://openweathermap.org/img/wn/${nextFiveForecast.weather[0].icon}@2x.png" width="35px
@@ -70,10 +70,10 @@ forcastElemnent.innerHTML +=
                   <strong>
                  ${Math.round(nextFiveForecast.temp.max)}°
                   </strong>
+                  </div>
                   <br/>
-                  ${Math.round(nextFiveForecast.temp.max)}° 
+                  ${Math.round(nextFiveForecast.temp.min)}° 
                 </p>
-          </div>
         </div>
       </div>`;
 
